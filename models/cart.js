2436f58e6e9951b5923cd689fe7c41b70e052014
zainabtalaq <zainab.altallaq@gmail.com>
2024-07-07 15:53:50 +0300

    create Registration Schema

--- models/cart.js
+++ models/cart.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
-
-const cartSchema = new Schema(
+const reviewSchema = new Schema(
   {
-    program: [{ type: Schema.Types.ObjectId, ref: "Program" }],
-    totalPrice: Number,
+    content: { type: String, required: true },
+    rating: { type: String, required: true },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
   {
     timestamps: true,
   }
 )
 
-module.exports = mongoose.model("Cart", cartSchema)
+module.exports = mongoose.model("Review", reviewSchema)
